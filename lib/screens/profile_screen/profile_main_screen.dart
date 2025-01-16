b75f95cc3403ce0cd8eb34406cd2b254685743be
srivigneshbalaji <srivignesh.s@zebuetrade.com>
2025-01-16 17:55:22 +0530

    1.0.56 TF hosted

--- lib/screens/profile_screen/profile_main_screen.dart
+++ lib/screens/profile_screen/profile_main_screen.dart
@@ -420,7 +420,7 @@ class UserAccountScreen extends ConsumerWidget {
       Container(
           margin: const EdgeInsets.only(bottom: 10),
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text("Version 3.0.2 Build 1.0.55(03) Released on 10 Jan",
+          child: Text("Version 3.0.2 Build 1.0.56(01) Released on 16 Jan",
               style: textStyle(const Color(0xff666666), 11, FontWeight.w500)))
     ]);
   }
