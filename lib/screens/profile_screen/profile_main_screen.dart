7249c49b785966a8571c597aebccb8582ef81fe3
srivigneshbalaji <srivignesh.s@zebuetrade.com>
2025-01-27 09:17:38 +0530

    testing build

--- lib/screens/profile_screen/profile_main_screen.dart
+++ lib/screens/profile_screen/profile_main_screen.dart
@@ -418,7 +418,7 @@ class UserAccountScreen extends ConsumerWidget {
       Container(
           margin: const EdgeInsets.only(bottom: 10),
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text("Version 3.0.2 Build 1.0.56(02) Released on 18 Jan",
+          child: Text("Version 3.0.2 Build 1.0.56(03) Released on 27 Jan",
               style: textStyle(const Color(0xff666666), 11, FontWeight.w500)))
     ]);
   }
