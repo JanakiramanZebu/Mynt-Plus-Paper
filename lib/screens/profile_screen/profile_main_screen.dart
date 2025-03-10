709ae507703a02aaf118e958701382dbf479bbc6
srivigneshbalaji <srivignesh.s@zebuetrade.com>
2025-03-10 10:30:23 +0530

    10032025 observation deployed

--- lib/screens/profile_screen/profile_main_screen.dart
+++ lib/screens/profile_screen/profile_main_screen.dart
@@ -458,7 +458,7 @@ class UserAccountScreen extends ConsumerWidget {
             Container(
                 margin: const EdgeInsets.only(bottom: 10),
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Text("Version 3.0.2 Build 1.0.66(02) Released on 10 Mar",
+                child: Text("Version 3.0.2 Build 1.0.67(01) Released on 10 Mar",
                     style: textStyle(
                         const Color(0xff666666), 11, FontWeight.w500)))
           ]);
